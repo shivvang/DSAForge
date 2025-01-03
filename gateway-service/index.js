fea549846d7bce026670b478cbb150e2c3599d8f
shivvang <shivanghariakar@gmail.com>
2025-01-03 18:48:32 +0530

    Updated routes in Gateway to correctly route requests to respective microservices using express-http-proxy

--- gateway-service/index.js
+++ gateway-service/index.js
@@ -7,14 +7,20 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
+//this is what express http proxy does
+
+//Forwards Requests: It sends incoming API requests from one server (Gateway) to another server (Microservices like User, Problem, Review).
+//Hides Internal Services: Clients only interact with the Gateway (8000), never seeing 8001, 8002, or 8003.
+//Simplifies Routing: You can define routes in the Gateway and let the proxy handle forwarding seamlessly.
+
 //proxy to user-service
 app.use('/user',proxy('http://localhost:8001'));
 
 //proxy to review-service
-app.use("/review",proxy('http://localhost:8002'));
+app.use("/reviews",proxy('http://localhost:8002'));
 
 //proxy to problem-service
-app.use('/',proxy('http://localhost:8003'));
+app.use('/problems',proxy('http://localhost:8003'));
 
 app.listen(8000,()=>{
 console.log(`listening to Gateway service on port 8000`)
