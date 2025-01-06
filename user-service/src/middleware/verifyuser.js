930faf7181c21f6b55f8004b10117f3870cc69a3
shivvang <shivanghariakar@gmail.com>
2025-01-06 16:47:25 +0530

    Refactor: Start fresh with a cleaner and more efficient approach

--- user-service/src/middleware/verifyuser.js
+++
@@ -1,5 +0,0 @@
-export const verifyUser = (req, res, next) => {
-    if (req.isAuthenticated()) return next();
-    console.log("im here at verify user");
-    return res.status(401).json({ message: "User not authenticated. Please log in." });
-};
